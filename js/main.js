b95ec68f541cd4274f917bed019349356119637c
IngvarMorozov <bujhtif11@gmail.com>
2020-10-20 21:15:49 +0300

    learned the basics of js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,85 @@
 'use strict';
 
-alert('Начало');
-const who = prompt('Кто там?');
-if (who === 'Отмена' || who === '' || who === null) {
-  alert('Отменено');
-} else if (who === 'Другое') {
-  alert('Я вас не знаю');
-} else if (who === 'Админ') {
-  const who = prompt('Пароль?');
-  if (who === 'Отмена' || who === '' || Boolean(who) === null) {
-    alert('Отменено');
-  } else if (who === 'Другое') {
-    alert('Неверный пароль');
-  } else if (who === 'Я Главный') {
-    alert('Здравствуйте!');
-  } else {
-    alert('Неверный пароль, начинайте сначала');
-  }
-} else {
-  alert('Идите нахуй!');
-}
+// function min(a, b) {
+//   if (a > b) {
+//     return b;
+//   } else if (a < b) {
+//     return a;
+//   }
+//   return a;
+// }
+// console.log(min(2, 1));
+// console.log(min(2, -1));
+// console.log(min(1, 1));
+
+// Functions
+// function pow(x, n = 2) {
+//   const temp = x ** n;
+//   return temp;
+// }
+// pow(2);
+
+// // Function Expression
+// const sayHi = function() {
+//   console.log('Hello World');
+// };
+
+// Function-"callback"
+// when we pass functions as value
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+// function showOk() {
+//   alert('You are agree');
+// }
+// function showCancel() {
+//   alert('You canceled ');
+// }
+
+// ask('Are you agree?', showOk, showCancel);
+
+// переопределение
+
+// const age = 16;
+
+// let welcome;
+
+// if (age < 18) {
+//   welcome = function () {
+//     console.log('hello');
+//   };
+//   welcome();
+// }
+// welcome();
+// welcome = function () {
+//   console.log('Hi');
+// };
+
+// welcome();
+
+// Arrow Functions
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+// function showOk() {
+//   alert('You are agree');
+// }
+// function showCancel() {
+//   alert('You canceled ');
+// }
+
+
+
+const ask = (question, yes, no) => {
+  if (confirm(question)) yes();
+  else no();
+};
+const showOk = () => alert('You are agree');
+const showCancel = () => alert('You canceled');
+
+ask('Are you agree?', showOk, showCancel);
+
