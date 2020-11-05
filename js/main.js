bcbca129851529f39623ff206e2721eccbbc4635
IngvarMorozov <bujhtif11@gmail.com>
2020-11-05 22:13:13 +0200

    learned strings + bonus - checkSpam()

--- js/main.js
+++ js/main.js
@@ -1,51 +1,18 @@
 'use strict';
 
-// Constructors, creating objects through "new"
+const spam = ['viagra', 'xxx', 'innocent'];
 
-// Constructor Function - is common function. But
-// 1. The name of the design function must start with a capital letter.
-// 2. The design function must be called with the "new" operator.
+function checkSpam(str) {
+  for (let i = 0; i < spam.length; i++) {
+    if (str.toLowerCase().includes(spam[i])) {
+      return true;
+    }
+  }
+  return false;
+}
+console.log(checkSpam('buy vIagra now'));
+console.log(checkSpam('free xxxxx'));
+console.log(checkSpam('innocent rabbit'));
 
-// Example
-// function User(name) {
-//   this.name = name;
-//   this.isAdmin = false;
-// }
-
-// const user = new User('Vasyl');
-
-// console.log(user.name);
-// console.log(user.isAdmin);
-
-// Calculator
-
-// function Calculator() {
-//   this.read = function () {
-//     this.a = Number(prompt('Please enter first number', 0));
-//     this.b = Number(prompt('Please enter second number', 0));
-//   };
-//   this.sum = function () {
-//     return this.a + this.b;
-//   };
-//   this.mul = function () {
-//     return this.a * this.b;
-//   };
-// }
-
-// const calculator = new Calculator();
-// calculator.read();
-// alert(`Sum = ${calculator.sum()}`);
-// alert(`Mul = ${calculator.mul()}`);
-
-// const calculator = {
-//   read() {
-//     this.a = Number(prompt('Please enter first number', 0));
-//     this.b = Number(prompt('Please enter second number', 0));
-//   },
-//   sum() {
-//     return this.a + this.b;
-//   },
-//   mul() {
-//     return this.a * this.b;
-//   }
-// }
+spam[length] = 'fuck';
+console.log(checkSpam('Igor fuck Max'));
