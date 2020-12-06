9a0e5cb7112677d74484a445ed28de734cb0183c
IngvarMorozov <bujhtif11@gmail.com>
2020-12-06 09:40:41 +0200

    learned Promise

--- js/main.js
+++ js/main.js
@@ -1,28 +1,66 @@
 'use strict';
 
-// function createCalcFunction(n) {
-//   return function() {
-//     console.log(1000 * n);
-//   }
-// }
-// const calc = createCalcFunction(42);
-// calc(30);
+// console.log('Request data...');
 
-// function createIncrementor(n) {
-//   return function(num){
-//     return n + num;
-//   }
-// }
-// const addOne = createIncrementor(1);
-// console.log(addOne(3));
+// setTimeout(() => {
+//   console.log('Preparing data...');
 
-function urlGenerator(domain) {
-  return function(url) {
-    return `https://${url}.${domain}`
-  }
-}
+//   const backendData = {
+//     server: 'aws',
+//     port: 2000,
+//     status: 'working',
+//   };
 
-const comUrl = urlGenerator('com')
-console.log(comUrl('google'));
-const uaUrl = urlGenerator('ua');
-console.log(uaUrl('dou'));
+//   setTimeout(() => {
+//     backendData.modified = true;
+//     console.log('Data received', backendData);
+//   }, 2000);
+// }, 2000);
+
+// resolve - викликається, коли операція завершена і успішно
+// reject -
+// const p = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     console.log('Preparing data...');
+//     const backendData = {
+//       server: 'aws',
+//       port: 2000,
+//       status: 'working',
+//     };
+//     resolve(backendData); // we say to Promise that it has completed his execution and pass the object to then
+//   }, 2000);
+// });
+
+// // then виконується коли закінчиться асинхронна операція
+// // chain - запис через точку,
+// p.then(data => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       data.modified = true;
+//       resolve(data);
+//     }, 2000);
+//   });
+//   // p2.then(clientData => {
+//   //   console.log('Data received', clientData);
+//   // });
+// }).then(clientData => {
+//   console.log('Data received', clientData);
+//   clientData.fromPromise = true;
+//   return clientData;
+// }).then(data => {
+//   console.log(data);
+// })
+// .catch(err => console.error('Error: ', err))
+// .finally(() => console.log('Finally'));
+
+// const sleep = ms => {
+//   return new Promise(resolve => setTimeout(() => resolve(), ms));
+// };
+// // sleep(2000).then(() => console.log('After 2 sec'));
+
+// Promise.all([sleep(2000), sleep(3000)]).then(() => {
+//   console.log('All promises');
+// });
+// Promise.race([sleep(2000), sleep(3000)]).then(() => {
+//   console.log('Race promises');
+// });
