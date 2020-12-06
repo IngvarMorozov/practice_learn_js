cc250e508b5812b5e36ad6f1f43607f465c20bae
IngvarMorozov <bujhtif11@gmail.com>
2020-12-06 10:13:19 +0200

    learnd Object.create

--- js/main.js
+++ js/main.js
@@ -1,66 +1,44 @@
 'use strict';
 
-// console.log('Request data...');
+const person = Object.create(
+  {
+    calculateAge() {
+      console.log('Age', new Date().getFullYear() - this.birthYear);
+    }
+  },
+  {
+    name: {
+      value: 'Vladilen',
+      enumerable: true,
+      writable: true,
+      configurable: true,
+    },
+    birthYear: {
+      value: 1993,
+      enumerable: false,
+      writable: false,
+      configurable: false,
+    },
+    age: {
+      get() {
+        return new Date().getFullYear() - this.birthYear;
+      },
+      set(value) {
+        document.body.style.background = 'red';
+        console.log('Set age', value);
+      },
+    },
+  }
+);
 
-// setTimeout(() => {
-//   console.log('Preparing data...');
+// person.name = 'Maxim'
 
-//   const backendData = {
-//     server: 'aws',
-//     port: 2000,
-//     status: 'working',
-//   };
+for (let key in person) {
+  if(person.hasOwnProperty(key)){
+    console.log('Key', key, person[key]);
+  }
+}
 
-//   setTimeout(() => {
-//     backendData.modified = true;
-//     console.log('Data received', backendData);
-//   }, 2000);
-// }, 2000);
 
-// resolve - викликається, коли операція завершена і успішно
-// reject -
-// const p = new Promise(function (resolve, reject) {
-//   setTimeout(() => {
-//     console.log('Preparing data...');
-//     const backendData = {
-//       server: 'aws',
-//       port: 2000,
-//       status: 'working',
-//     };
-//     resolve(backendData); // we say to Promise that it has completed his execution and pass the object to then
-//   }, 2000);
-// });
 
-// // then виконується коли закінчиться асинхронна операція
-// // chain - запис через точку,
-// p.then(data => {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       data.modified = true;
-//       resolve(data);
-//     }, 2000);
-//   });
-//   // p2.then(clientData => {
-//   //   console.log('Data received', clientData);
-//   // });
-// }).then(clientData => {
-//   console.log('Data received', clientData);
-//   clientData.fromPromise = true;
-//   return clientData;
-// }).then(data => {
-//   console.log(data);
-// })
-// .catch(err => console.error('Error: ', err))
-// .finally(() => console.log('Finally'));
-
-// const sleep = ms => {
-//   return new Promise(resolve => setTimeout(() => resolve(), ms));
-// };
-// // sleep(2000).then(() => console.log('After 2 sec')); 
-
-// Promise.all([sleep(2000), sleep(3000)]).then(() => {
-//   console.log('All promises');
-// });
-// Promise.race([sleep(2000), sleep(3000)]).then(() => {
-//   console.log('Race promises');
-// });
+// console.log(person);
